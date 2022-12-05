faabccfe146cfea1541d1bc704a966e94c2d7584
DarioDC95 <dario_95@live.com>
2022-12-05 22:31:04 +0100

    Corrected function rundom_num (min, max)

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -2,7 +2,7 @@
 function random_num(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 let somma;
